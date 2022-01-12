07a584c4294c2f762014df2801689149643545c6
Alex Bolton <boltonalex89@gmail.com>
2022-01-12 17:06:25 +0000

    Implemented initial express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const router = require('./router');
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
