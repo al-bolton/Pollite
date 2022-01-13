7f9f230db2c4c57f5761f84ea4e86ca971270f69
Alex Bolton <boltonalex89@gmail.com>
2022-01-13 13:42:44 +0000

    Implemented create poll page with date selection

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  // Redirect from index straight to create poll, todo REMOVE AFTER INDEX CREATION
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/create',
+        permanent: true,
+      },
+    ]
+  },
 }
